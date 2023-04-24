5ac1c373624cc009077972bdc03c376178c2eee7
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-04-24 11:23:57 -0400

    add seeds to db and routes to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,27 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/reservations" do
+    vehicles = Vehicle.all
+    vehicles.to_json
+  end
+
+  #post 
+  get "/reservations" do
+    vehicles = Vehicle.create()
+    vehicles.to_json
+  end
+
+  #update
+  get "/reservations/:id" do
+    vehicle = Vehicle.find(params[:id])
+    vehicle.update
+  end
+
+  #delete
+  get "/reservations/:id" do
+    vehicles = Vehicle.find(params[:id])
+    vehicles.destroy
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,23 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding vehicles..."
 
 # Seed your database here
+50.times do
+    # create a game with random data
+    Vehicle.create(
+      vin: Faker::Vehicle.vin,
+      car_type: Faker::Vehicle.car_type,
+      make_and_model: Faker::Vehicle.make_and_model,
+      license_plate: Faker::Vehicle.license_plate
+    )
+  end
+
+  22.times do
+    Reservation.create(
+        full_name: Faker::Name.name ,
+        driving_license: Faker::DrivingLicence.usa_driving_licence, 
+        payment_method: Faker::Finance.credit_card || "cash",
+    )
+
+  end
 
 puts "✅ Done seeding!"
