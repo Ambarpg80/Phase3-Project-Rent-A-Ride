9440249bf702a718b7f9174b52cab94683455430
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-04-25 00:14:37 -0400

    adjust routes to get 2 models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,30 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/reservations" do
+  get "/vehicles" do
     vehicles = Vehicle.all
     vehicles.to_json
   end
 
+  get "/reservations" do
+    reservation = Reservation.all
+    reservation.to_json
+  end
+
   #post 
   get "/reservations" do
-    vehicles = Vehicle.create()
-    vehicles.to_json
+    reservation = Reservation.create(
+              full_name: params[:full_name],
+              driving_license: params[:driving_license],
+              payment_method: params[:payment_method],
+              vehicle_id: params[:vehicle_id]
+    )
+    reservation.to_json
   end
 
   #update
   get "/reservations/:id" do
-    vehicle = Vehicle.find(params[:id])
+    vehicle = Reservation.find(params[:id])
     vehicle.update
   end
 
