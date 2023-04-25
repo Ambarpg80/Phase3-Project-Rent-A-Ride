66820d695c55501ca06b1299d94e6e1a74d3288e
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-04-25 00:16:42 -0400

    add method for seed info and adjust migration file

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,9 +1,13 @@
+
 class Reservation < ActiveRecord::Base
 	belongs_to :vehicles
 
-    def vehicle_id
-        vehicles.rand(:id)
+     def self.add_vid  #return a random vehicle id
+        # binding.pry
+        ran_num = Vehicle.all.map {|vehicle| vehicle.id}
+        ran_num.sample(1)
     end
 
    
 end
+
--- db/migrate/20230423210128_create_reservations.rb
+++ db/migrate/20230423210128_create_reservations.rb
@@ -2,7 +2,7 @@ class CreateReservations < ActiveRecord::Migration[6.1]
   def change
     create_table :reservations do |t|
       t.string :full_name 
-      t.boolean :driving_license    #(: false :null)
+      t.string :driving_license    #(: false :null)
       t.string :payment_method #(cash/card)
       t.integer :vehicle_id     #(: false :null)
       t.timestamps
