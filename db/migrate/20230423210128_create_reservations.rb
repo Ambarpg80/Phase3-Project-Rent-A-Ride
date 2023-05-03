9e80d235fc49ac9eb0bc737d5a5965ecaaf01dfe
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-05-03 10:41:57 -0400

    adjust vehicles table association

--- db/migrate/20230423210128_create_reservations.rb
+++ db/migrate/20230423210128_create_reservations.rb
@@ -1,7 +1,7 @@
 class CreateReservations < ActiveRecord::Migration[6.1]
   def change
     create_table :reservations do |t|
-      t.belongs_to :vehicle, foreign_key: true 
+      t.belongs_to :vehicles, foreign_key: true
       t.string :full_name 
       t.string :driving_license    
       t.string :payment_method 
