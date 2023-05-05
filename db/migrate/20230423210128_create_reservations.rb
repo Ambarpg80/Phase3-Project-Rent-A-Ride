28d6b72f4e05e522f63b447eaadd118fbd8e68dd
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-05-05 17:53:43 -0400

    remove pluralization created by initial foreign key creation

--- db/migrate/20230423210128_create_reservations.rb
+++ db/migrate/20230423210128_create_reservations.rb
@@ -1,12 +1,13 @@
 class CreateReservations < ActiveRecord::Migration[6.1]
   def change
     create_table :reservations do |t|
-      t.belongs_to :vehicles, foreign_key: true
+      t.belongs_to :vehicle, foreign_key: true, column: "vehicle_id"
       t.string :full_name 
       t.string :driving_license    
       t.string :payment_method 
       t.timestamps
     end
-    
   end
+  
+
 end
