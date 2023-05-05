013bece55a3efc90cfc740de98dd7b5e9114cbdd
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-05-05 17:55:04 -0400

    adjust sample data to include vehicle_id and reserved table info

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,25 @@ puts "🌱 Seeding reservations..."
       car_type: Faker::Vehicle.car_type,
       make_and_model: Faker::Vehicle.make_and_model,
       license_plate: Faker::Vehicle.license_plate,
-      reserved: false
+      # reserved: false
     )
   end
-
-
 
   4.times do
     Reservation.create(
         full_name: Faker::Name.name ,
         driving_license: Faker::DrivingLicence.usa_driving_licence, 
         payment_method: Faker::Finance.credit_card,
-        vehicles_id: Reservation.add_vid
-      )
-    Vehicle.update(
-      reserved: Vehicle.is_reserved?(:id)
-    ) 
-  end
+        vehicle_id: Reservation.add_vid
+      ) 
+    end
+    
+  Vehicle.all.each{|car|
+      car.update(reserved: Reservation.exists?(vehicle_id: car.id))
+    }
+
+     
+ 
   
   
  
