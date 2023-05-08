3375b04dd268603c69dc523fcad5aad55cc2be5d
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-05-08 12:35:19 -0400

    updated methods to perform CRUD actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,45 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  # show all vehicles
   get "/vehicles" do
     vehicles = Vehicle.all
     vehicles.to_json(include: :reservations)
   end
 
+  #show reservations
   get "/vehicles/reservations" do
     reservation = Reservation.all
     reservation.to_json
   end
 
-  #post 
-  get "/vehicles/reservations" do
+  #post new reservation
+  post "/vehicles/reservations" do
     reserve = Reservation.create(
+              vehicle_id: params[:vehicle_id],
               full_name: params[:full_name],
               driving_license: params[:driving_license],
-              payment_method: params[:payment_method],
-              vehicle_id: params[:vehicle_id]
+              payment_method: params[:payment_method]
     )
     # reserve = Reservation.create(params)
     reserve.to_json
   end
 
-  #update
-  get "/vehicles/reservations/:id" do
+  #update reservation
+  patch "/vehicles/reservations/:id" do
     vehicle = Reservation.find(params[:id])
-    vehicle.update
+    vehicle.update(vehicle_id: params[:vehicle_id],
+                    full_name: params[:full_name],
+                    driving_license: params[:driving_license],
+                    payment_method: params[:payment_method]
+    )
   end
 
-  #delete
-  get "/vehicles/reservations/:id" do
-    vehicles = Vehicle.find(params[:id])
+  #delete reservation
+  delete "/vehicles/reservations/:id" do
+    vehicles = Reservation.find(params[:id])
     vehicles.destroy
+    vehicles.to_json
   end
 
 end
